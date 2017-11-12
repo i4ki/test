b3bc53cfab54c6e4121cab3a9480d61debaff3cb
i4k <tiago4orion@gmail.com>
2017-11-12 20:31:58 -0200

    fix identations
    Signed-off-by: i4k <tiago4orion@gmail.com>

--- test.sh
+++ test.sh
@@ -1,48 +1,48 @@
 import log
 
 fn newctx() {
-    var t = map{
-        shouldExit: false,
-        succeeded: false,
-        failureCause: "",
-    }
+	var t = map{
+		shouldExit: false,
+		succeeded: false,
+		failureCause: "",
+	}
 
-    t.fatalf = fn(msg, ...args) {
-        log.errorf(msg, args...)
-        t.shouldExit = true
-    }
+	t.fatalf = fn(msg, ...args) {
+		log.errorf(msg, args...)
+		t.shouldExit = true
+	}
 
 	t.assert = fn(msg, b) {
 		if s1 == s2 {
-            t.succeeded = true
-        } else {
-            t.succeeded = false 
-            t.failureCause = msg
-        }
-        return t
+			t.succeeded = true
+		} else {
+			t.succeeded = false 
+			t.failureCause = msg
+		}
+		return t
 	}
 
-    t.assertstr = fn(msg, s1, s2) {
-        return t.assert(msg, s1 == s2)
-    }
+	t.assertstr = fn(msg, s1, s2) {
+		return t.assert(msg, s1 == s2)
+	}
 
-    return t
+	return t
 }
 
 fn run(desc, testfn) {
-    log.printf("RUN %s: ", desc)
-    ctx = testfn(newctx())
-    if ctx.succeeded {
-        log.printf("ok\n")
-        return true
-    }
+	log.printf("RUN %s: ", desc)
+	ctx = testfn(newctx())
+	if ctx.succeeded {
+		log.printf("ok\n")
+		return true
+	}
 
-    log.printf("failed\n")
-    log.printf("cause (%s): %s", desc, ctx.failureCause)
-    if ctx.shouldExit {
-        exit(1)
-    }
-    return false
+	log.printf("failed\n")
+	log.printf("cause (%s): %s", desc, ctx.failureCause)
+	if ctx.shouldExit {
+		exit(1)
+	}
+	return false
 }
 
 fn runall(tests) {
